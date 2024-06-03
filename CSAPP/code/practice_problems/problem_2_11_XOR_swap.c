ed159e68f66b702b634d60594d2539370a9d38fc
root <root@localhost.localdomain>
2024-06-03 17:26:55 +0800

    problem 2.11

---
+++ CSAPP/code/practice_problems/problem_2_11_XOR_swap.c
@@ -0,0 +1,41 @@
+# include<stdio.h>
+
+void inplace_swap(int *x, int *y) 
+{
+
+	*y = *x ^ *y;
+	*x = *x ^ *y;
+	*y = *x ^ *y;
+
+}
+
+
+void reverse_array(int a[], int cnt) 
+{
+	int first, last;
+	for (first = 0, last = cnt - 1; 
+			first <= last; 
+			first++, last--) {
+		
+		inplace_swap(&a[first], &a[last]);
+	
+	}
+
+}
+
+
+int main(void) 
+{
+	int a[] = {1, 2, 3, 4, 5};
+	int cnt = 5;
+
+	reverse_array(a, cnt);
+
+	int i;
+	for (i = 0; i < cnt; i++) {
+		printf("%d, ", a[i]);
+	}
+	printf("\n");
+
+	return 0;
+}
