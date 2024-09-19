9fec7305821cf502fcb799042b4e9c751ed1a3f9
Tom <wuyangzerg@gmail.com>
2024-09-19 17:38:34 +0800

    finish parctice problem 2.42

--- CSAPP/code/practice_problems/p2.42_division_of_16.c
+++ CSAPP/code/practice_problems/p2.42_division_of_16.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
+/*
+ * In my opinion, this problem help us to understand how the compiler deal with the calculation
+ * of a negative number divided by a divisor.
+ *
+ * What the function 'div16' does is exactly as same as a comiler does in a low level.
+ *
+ * N.B. A negative number such as -38 plus a bias(e.g. 15) is -23 which is larger.
+ *
+ * */
+
+/*
+ * Write a function to impement division by 16(2^4, >> 4).
+ * */
 int div16(int x) 
 {
-	return x >> 4;
+	//16 - 1 is 0xF.
+	int bias = x >> 31 & 0xF;
+	return (x + bias) >> 4;
 }
 
 int main(void) 
