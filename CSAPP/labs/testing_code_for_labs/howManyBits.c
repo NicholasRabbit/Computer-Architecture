a5b743f6b8ff178366c8ee106759853968ae6b15
Tom <wuyangzerg@gmail.com>
2025-03-26 17:21:30 +0800

    test code for howManyBits

--- CSAPP/labs/testing_code_for_labs/howManyBits.c
+++ CSAPP/labs/testing_code_for_labs/howManyBits.c
@@ -1,26 +1,37 @@
 #include <stdio.h>
 
 int howManyBits(int x);
+char howManyBitsChar(char x);
 
 
 int main(void) 
 {
-	printf("Plea input a number: \n");
+	printf("Please input a number: \n");
 	int num;
-	scanf("%d", &num);
-	num = howManyBits(num);
+	scanf("%x", &num);
+	//num = howManyBits(num);
+	num = howManyBitsChar(num);
 	printf("How many bits? ==> %d\n", num);
 	return 0;
 }
 
 int howManyBits(int x) 
 {
-	x = x | x >> 1;
-	x = x | x >> 2;
-	x = x | x >> 4;
-	x = x | x >> 8;
-	x = x | x >> 16;
-	
-	return (x + 1) >> 1;
+	int sign = x >> 31;
+	int abs_x = (sign & ~x);
+	int abs_y = (~sign & x);
+
+	return abs_x;
 
 }
+
+char howManyBitsChar(char x) 
+{
+	char sign = x >> 7;
+	char abs_x = sign & ~x;
+	char abs_y = ~sign & x;
+	char result = abs_x | abs_y;
+	return result;
+}
+
+
