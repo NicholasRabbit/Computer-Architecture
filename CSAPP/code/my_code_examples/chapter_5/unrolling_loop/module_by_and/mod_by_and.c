9eb2bb895dd066a0a24b387975ec75b808f04315
Tom <wuyangzerg@gmail.com>
2026-07-02 19:38:21 +0800

    add loop unrolling to ncopy.ys

--- CSAPP/code/my_code_examples/chapter_5/unrolling_loop/module_by_and/mod_by_and.c
+++ CSAPP/code/my_code_examples/chapter_5/unrolling_loop/module_by_and/mod_by_and.c
@@ -2,5 +2,20 @@
 
 int mod_by_and(int m, int n)
 {
-	return n % m;
+	return n & m;
+}
+
+int main(int argc, char *argv[])
+{
+
+	int m, n;
+	m = 3;
+	n = 30;
+	int i;
+	for (i = 1; i < n; i++) {
+		int r = mod_by_and(m, i);
+		int mod = i % (m + 1);
+		printf("%d %% %d = %d ?= %d\n", i, m, r, mod);
+	}
+	return 0;
 }
